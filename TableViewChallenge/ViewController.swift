a9345f1ea7e9075232dcd1ac254d6516fed3db01
Leonardo Reis <leonardobsr@gmail.com>
2019-04-09 17:31:52 -0300

    update

--- TableViewChallenge/ViewController.swift
+++ TableViewChallenge/ViewController.swift
@@ -40,11 +40,27 @@ extension ViewController: UITableViewDataSource {
 //        cell.nameLabel.text = cat.name
         cell.sessionCellView.layer.cornerRadius = 60
         cell.sessionCellView.layer.maskedCorners = [.layerMinXMaxYCorner,.layerMinXMinYCorner]
-        cell.sessionCellView.backgroundColor = .red
+        cell.sessionCellView.backgroundColor = #colorLiteral(red: 0.2980392157, green: 0.3058823529, blue: 0.6274509804, alpha: 1)
         cell.sessionImageButton.layer.cornerRadius = 60
 //        cell.sessionImageView.image = UIImage(named: "DorianLewisThumb")
-        cell.sessionNameLabel.text = "teste"
+        cell.sessionNameLabel.attributedText = labelWithImage(image: "Caminho 22", label: "Who buys your game?")
+        cell.sessionIdTimeLabel.text = "Session: 5 (20 min)"
+        cell.sessionSpeakerJobLabel.text = "Darian Lewis - EA Games"
+    
+        cell.sessionTimeLabel.attributedText = labelWithImage(image: "Agrupar 300", label: "14 pm")
+        cell.sessionLocalizationLabel.attributedText = labelWithImage(image: "Agrupar 302", label: "Hall 2")
+        cell.sessionKnowMoreButton.titleLabel?.text = "know more"
         return cell
+    }
+    
+    func labelWithImage(image: String, label: String) -> NSAttributedString {
+        let fullString = NSMutableAttributedString(string: "")
+        let image1Attachment = NSTextAttachment()
+        image1Attachment.image = UIImage(named: image)
+        let image1String = NSAttributedString(attachment: image1Attachment)
+        fullString.append(image1String)
+        fullString.append(NSAttributedString(string: " " + label))
+        return fullString
     }
 }
 
